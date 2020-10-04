27d755d34ee0a0e42093ced1a888582899ffcfdf
Mads Meinert Andersen <mads485d@edu.easj.dk>
2020-10-04 20:02:26 +0200

    StringUtility + BowlingGame

--- src/test/java/StringUtility/StringUtilityTest.java
+++ src/test/java/StringUtility/StringUtilityTest.java
@@ -1,14 +1,37 @@
 package StringUtility;
 
 
+import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringUtilityTest {
+    private StringUtility stringUtility;
+
+    @BeforeEach
+    protected void setUp(){
+        stringUtility = new StringUtility();
+    }
+    @Test
+    public void ReverseStringTesta(){
+        //Act
+        String actual = stringUtility.ReverseString("a");
+        String expected = "a";
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void ReverseStringTestaB(){
+        //Act
+        String actual = stringUtility.ReverseString("aB");
+        String expected = "Ba";
+        //Assert
+        assertEquals(expected, actual);
+    }
 
     @Test
-    public void ReverseStringTest(){
-        //Arrange
-        StringUtility stringUtility = new StringUtility();
+    public void ReverseStringTestaBc(){
         //Act
         String actual = stringUtility.ReverseString("aBc");
         String expected = "cBa";
@@ -16,20 +39,47 @@ public class StringUtilityTest {
         assertEquals(expected, actual);
     }
     @Test
-    public void CapitalizeStringTest(){
-        //Arrange
-        StringUtility stringUtility = new StringUtility();
+    public void CapitalizeStringTesta(){
+        //Act
+        String actual = stringUtility.CapitalizeString("a");
+        String expected = "A";
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void CapitalizeStringTestaB(){
+        //Act
+        String actual = stringUtility.CapitalizeString("aB");
+        String expected = "AB";
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void CapitalizeStringTestaBc(){
         //Act
         String actual = stringUtility.CapitalizeString("aBc");
         String expected = "ABC";
         //Assert
         assertEquals(expected, actual);
     }
-
     @Test
-    public void LowercaseStringTest(){
-        //Arrange
-        StringUtility stringUtility = new StringUtility();
+    public void LowercaseStringTesta(){
+        //Act
+        String actual = stringUtility.LowercaseString("a");
+        String expected = "a";
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void LowercaseStringTestaB(){
+        //Act
+        String actual = stringUtility.LowercaseString("aB");
+        String expected = "ab";
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void LowercaseStringTestaBc(){
         //Act
         String actual = stringUtility.LowercaseString("aBc");
         String expected = "abc";
